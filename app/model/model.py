d2cbbf67b4ac52c382d846310440daf6d26ef9e5
Kuba <kubaz11011@gmail.com>
2023-09-25 19:54:09 +0200

    tweak ML model

--- app/model/model.py
+++ app/model/model.py
@@ -1,16 +1,13 @@
 import pickle
 import re
-from pathlib import Path
 
-__version__ = "0.1.0"
+# Load the trained model
+model_path = f"ml-project-ue-katowice.pkl"
 
-BASE_DIR = Path(__file__).resolve(strict=True).parent
-
-
-with open(f"{BASE_DIR}/trained_pipeline-{__version__}.pkl", "rb") as f:
+with open(model_path, "rb") as f:
     model = pickle.load(f)
 
-
+# Define the classes
 classes = [
     "Arabic",
     "Danish",
@@ -23,18 +20,20 @@ classes = [
     "Italian",
     "Kannada",
     "Malayalam",
-    "Portugeese",
+    "Portuguese",
     "Russian",
     "Spanish",
-    "Sweedish",
+    "Swedish",
     "Tamil",
     "Turkish",
 ]
 
-
 def predict_pipeline(text):
+    # Preprocess the text
     text = re.sub(r'[!@#$(),\n"%^*?\:;~`0-9]', " ", text)
-    text = re.sub(r"[[]]", " ", text)
+    text = re.sub(r"\[.*?\]", " ", text)
     text = text.lower()
+
+    # Predict the class for the input text
     pred = model.predict([text])
     return classes[pred[0]]
