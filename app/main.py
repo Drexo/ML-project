cad79fb45aec319cb3957bdade228bd3f4f3f3f8
Kuba <kubaz11011@gmail.com>
2023-09-26 18:07:07 +0200

    fix main function bug

--- app/main.py
+++ app/main.py
@@ -1,24 +1,18 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from app.model.model import predict_pipeline
-from app.model.model import __version__ as model_version
-
 
 app = FastAPI()
-
 
 class TextIn(BaseModel):
     text: str
 
-
 class PredictionOut(BaseModel):
     language: str
 
-
 @app.get("/")
 def home():
-    return {"health_check": "OK", "model_version": model_version}
-
+    return {"health_check": "OK"}
 
 @app.post("/predict", response_model=PredictionOut)
 def predict(payload: TextIn):
